26679625de55a34eb309a07f41d343f1529332d3
ofirgalcon <ofir@gal.co.uk>
2025-04-22 20:02:19 +0100

    Update backblaze_tab.php
    List and report buttons in tab

--- views/backblaze_tab.php
+++ views/backblaze_tab.php
@@ -1,37 +1,53 @@
 <div id="backblaze-tab"></div>
-<h2 data-i18n="backblaze.title"></h2>
+<div id="lister" style="font-size: large; float: right;">
+    <a href="/show/listing/backblaze/backblaze" title="List">
+        <i class="btn btn-default tab-btn fa fa-list"></i>
+    </a>
+</div>
+<div id="report_btn" style="font-size: large; float: right;">
+    <a href="/show/report/backblaze/backblaze" title="Report">
+        <i class="btn btn-default tab-btn fa fa-th"></i>
+    </a>
+</div>
+<h2><i class="fa fa-cloud-upload"></i> <span data-i18n="backblaze.title"></span></h2>
 
 <table id="backblaze-tab-table" style="max-width:475px;"><tbody></tbody></table>
 
 <script>
 $(document).on('appReady', function(){
+    var table = $('#backblaze-tab-table');
     $.getJSON(appUrl + '/module/backblaze/get_data/' + serialNumber, function(data){
-        var table = $('#backblaze-tab-table');
-        $.each(data, function(key,val){
+        var rows = [];
+        $.each(data, function(key, val){
             var th = $('<th>').text(i18n.t('backblaze.column.' + key));
-            var td = $('<td>').text(val);
-            if (key === 'lastbackupcompleted') {
-                td = $('<td>').text(moment.unix(val).fromNow());
-            } else if (key === 'safety_frozen') {
-                td = $('<td>').text(val == 1 ? "Yes" : "No");
-                if (val == 1) {
-                    td.css('color', 'red');
-                }
-            } else if (key === 'encrypted') {
-                td = $('<td>').text(val == 1 ? "Yes" : "No");
-            } else if (key === 'remainingnumbytesforbackup') {
-                td = $('<td>').text(fileSize(val,2));
-            } else if (key === 'totnumbytesforbackup') {
-                td = $('<td>').text(fileSize(val,2));
-            } else if (key === 'remainingnumfilesforbackup') {
-                td = $('<td>').text(addThousandsSeparator(val));
-            } else if (key === 'totnumfilesforbackup') {
-                td = $('<td>').text(addThousandsSeparator(val));
-            } else {
-                td = $('<td>').text(val);
+            var td;
+            switch (key) {
+                case 'lastbackupcompleted':
+                    td = $('<td>').text(moment.unix(val).fromNow());
+                    break;
+                case 'safety_frozen':
+                    td = $('<td>').text(val == 1 ? "Yes" : "No");
+                    if (val == 1) {
+                        td.css('color', 'red');
+                    }
+                    break;
+                case 'encrypted':
+                    td = $('<td>').text(val == 1 ? "Yes" : "No");
+                    break;
+                case 'remainingnumbytesforbackup':
+                case 'totnumbytesforbackup':
+                    td = $('<td>').text(fileSize(val, 2));
+                    break;
+                case 'remainingnumfilesforbackup':
+                case 'totnumfilesforbackup':
+                    td = $('<td>').text(addThousandsSeparator(val));
+                    break;
+                default:
+                    td = $('<td>').text(val);
             }
-            table.append($('<tr>').append(th, td));
+            rows.push($('<tr>').append(th, td));
         });
+        table.append(rows);
     });
 });
 
@@ -39,7 +55,7 @@ function fileSize(size, decimals){
 	// Check if number
 	if(!isNaN(parseFloat(size)) && isFinite(size)){
 		if(size == 0){ return '0 B'}
-		if(decimals == undefined){decimals = 0};
+		if(decimals == undefined){decimals = 0}
 		var i = Math.floor( Math.log(size) / Math.log(1000) );
 		return ( size / Math.pow(1000, i) ).toFixed(decimals) * 1 + ' ' + ['', 'K', 'M', 'G', 'T', 'P', 'E'][i] + 'B';
 	}
